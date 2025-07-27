6e830eada8ba9c91f283ece627926ea3cf4cdfe5
gpsdick <gpsdick@gmail.com>
2025-07-27 20:25:02 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
@@ -38,12 +37,18 @@ int main() {
 
     char cidade1[51], cidade2[51]; // reserva um caractere a mais para armazenar o \n
 
-    int populacao1, populacao2, pontos1, pontos2; // numeros inteiros
+    unsigned long int populacao1, populacao2; // numeros inteiros grandes e positivos
+    
+    int pontos1, pontos2; // numeros inteiros
 
     float area1, area2, pib1, pib2; // numeros com digitos decimais
 
+    float densidade1, densidade2, percapita1, percapita2; // para armazenar resultados de calculos
+
+    float superpoder1, superpoder2; // para armazenar a soma das variaveis numericas
+
     //---fim da secao escrita pelo aluno---
-    
+
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
@@ -66,7 +71,7 @@ int main() {
     fgets(cidade1, 50, stdin); // le a quantidade de caracteres da cidade1
 
     printf("Quantos habitantes tem a Cidade 1? "); // exibe nova pergunta na tela
-    scanf("%d", &populacao1); // aguarda digitar o numero inteiro
+    scanf("%lu", &populacao1); // aguarda digitar o numero inteiro
 
     printf("Quantos quilometros quadrados tem a area da Cidade 1? "); // nova pergunta
     scanf("%f", &area1); // aguarda digitar o numero com duas casas decimais
@@ -95,7 +100,7 @@ int main() {
     fgets(cidade2, 51, stdin); // le a quantidade de caracteres da cidade2
 
     printf("Quantos habitantes tem a Cidade 2? "); // exibe nova pergunta na tela
-    scanf("%d", &populacao2); // aguarda digitar o numero inteiro
+    scanf("%lu", &populacao2); // aguarda digitar o numero inteiro
 
     printf("Quantos quilometros quadrados tem a area da Cidade 2? "); // nova pergunta
     scanf("%f", &area2); // aguarda digitar o numero com duas casas decimais
@@ -105,9 +110,20 @@ int main() {
 
     printf("Quantos pontos turisticos tem a Cidade 2? "); // nova pergunta
     scanf("%d", &pontos2); // aguarda digitar o numero inteiro
-    
+
+    // secao de calculos da Densidade Populacional e PIB per Capita
+
+    densidade1 = (float) populacao1 / area1;
+    densidade2 = (float) populacao2 / area2;
+
+    percapita1 = (float) pib1 * 1000000000 / populacao1;
+    percapita2 = (float) pib2 * 1000000000 / populacao2;
+
+    superpoder1 = (float) populacao1 + area1 + (pib1 * 1000000000) + pontos1 - densidade1 + percapita1;
+    superpoder2 = (float) populacao2 + area2 + (pib2 * 1000000000) + pontos2 - densidade2 + percapita2;
+
     //---fim da secao escrita pelo aluno---
-    
+
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
@@ -120,23 +136,93 @@ int main() {
     printf("Estado: %c\n", estado1);
     printf("Codigo: %s\n", carta1);
     printf("Nome da Cidade: %s", cidade1);
-    printf("Populacao: %d\n", populacao1);
+    printf("Populacao: %lu\n", populacao1);
     printf("Area: %.2f km2\n", area1);
     printf("PIB: %.2f bilhoes de reais\n", pib1);
-    printf("Numero de Pontos Turisticos: %d", pontos1);
+    printf("Numero de Pontos Turisticos: %d\n", pontos1);
+    printf("Densidade Populacional: %.2f hab./km2\n", densidade1);
+    printf("PIB per Capita: %.2f de reais\n", percapita1);
+    printf("Super Poder: %.2f\n", superpoder1);
 
     // exibicao dos dados da Carta 2
 
-    printf("\n\nCarta 2:\n");
+    printf("\nCarta 2:\n");
     printf("Estado: %c\n", estado2);
     printf("Codigo: %s\n", carta2);
     printf("Nome da Cidade: %s", cidade2);
-    printf("Populacao: %d\n", populacao2);
+    printf("Populacao: %lu\n", populacao2);
     printf("Area: %.2f km2\n", area2);
     printf("PIB: %.2f bilhoes de reais\n", pib2);
-    printf("Numero de Pontos Turisticos: %d\n\n", pontos2);
+    printf("Numero de Pontos Turisticos: %d\n", pontos2);
+    printf("Densidade Populacional: %.2f hab./km2\n", densidade2);
+    printf("PIB per Capita: %.2f de reais\n", percapita2);
+    printf("Super Poder: %.2f\n", superpoder2);
+
+    printf("\nComparacao das Cartas:\n");
+
+    printf("Populacao: Carta %s",(populacao1 > populacao2) ? "1 venceu (1)" : "2 venceu (0)");
+    printf("\nArea: Carta %s",(area1 > area2) ? "1 venceu (1)" : "2 venceu (0)");
+    printf("\nPIB: Carta %s",(pib1 > pib2) ? "1 venceu (1)" : "2 venceu (0)");
+    printf("\nPontos Turisticos: Carta %s",(pontos1 > pontos2) ? "1 venceu (1)" : "2 venceu (0)");
+    printf("\nDensidade Populacional: Carta %s",(densidade1 < densidade2) ? "1 venceu (1)" : "2 venceu (0)");
+    printf("\nPIB per Capita: Carta %s",(percapita1 > percapita2) ? "1 venceu (1)" : "2 venceu (0)");
+    printf("\nSuper Poder: Carta %s",(superpoder1 > superpoder2) ? "1 venceu (1)" : "2 venceu (0)");
+    printf("\n\n");    
 
     //---fim da secao escrita pelo aluno---
 
     return 0;
 }
+
+/* Entradas e saidas no terminal:
+
+Digite uma letra de 'A' ate 'H' para o Estado 1: A
+Digite o codigo de 01 a 04 para a Carta 1: A01
+Qual eh o nome da Cidade 1? São Paulo
+Quantos habitantes tem a Cidade 1? 12325000
+Quantos quilometros quadrados tem a area da Cidade 1? 1521.11
+Qual eh o valor do Produto Interno Bruto da Cidade 1? 699.28
+Quantos pontos turisticos tem a Cidade 1? 50
+
+Digite uma letra de 'A' ate 'H' para o Estado 2: B
+Digite o codigo de 01 a 04 para a Carta 2: B02
+Qual eh o nome da Cidade 2? Rio de Janeiro
+Quantos habitantes tem a Cidade 2? 6748000
+Quantos quilometros quadrados tem a area da Cidade 2? 1200.25
+Qual eh o valor do Produto Interno Bruto da Cidade 2? 300.50
+Quantos pontos turisticos tem a Cidade 2? 30
+
+Carta 1:
+Estado: A
+Codigo: A01
+Nome da Cidade: São Paulo
+Populacao: 12325000
+Area: 1521.11 km2
+PIB: 699.28 bilhoes de reais
+Numero de Pontos Turisticos: 50
+Densidade Populacional: 8102.64 hab./km2
+PIB per Capital: 56736.71 reais
+Super Poder: 699292385280.00
+
+Carta 2:
+Estado: B
+Codigo: B02
+Nome da Cidade: Rio de Janeiro
+Populacao: 6748000
+Area: 1200.25 km2
+PIB: 300.50 bilhoes de reais
+Numero de Pontos Turisticos: 30
+Densidade Populacional: 5622.16 hab./km2
+PIB per Capital: 44531.71 reais
+Super Poder: 300506775552.00
+
+Comparacao das Cartas:
+Populacao: Carta 1 venceu (1)
+Area: Carta 1 venceu (1)
+PIB: Carta 1 venceu (1)
+Pontos Turisticos: Carta 1 venceu (1)
+Densidade Populacional: Carta 2 venceu (0)
+PIB per Capita: Carta 1 venceu (1)
+Super Poder: Carta 1 venceu (1)
+
+*/
